329e66c29f17659a39a06355ea2ce7924dfb3467
Jacob Cohen <130943810+ConfluxConflux@users.noreply.github.com>
2026-03-24 00:57:15 -0700

    Auto-save: 2026-03-24 00:57

--- widgets/urls.py
+++ widgets/urls.py
@@ -5,7 +5,7 @@ app_name = 'widgets'
 
 urlpatterns = [
     path('', views.estimator, name='home'),
-    path('estimator/', views.estimator, name='estimator'),
+    path('time-calculator/', views.estimator, name='estimator'),
     path('focus-write/', views.focus_write, name='focus_write'),
     path('word-counter/', views.word_counter, name='word_counter'),
     path('advice/', views.advice, name='advice'),
